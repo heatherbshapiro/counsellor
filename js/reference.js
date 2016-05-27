834d497e52d1449c44776d9416172d2eaf46240c
stacey <BitchWhoCodes@STACEYLs-MacBook-Pro.local>
2016-05-26 21:20:10 -0400

    adding refernce js

--- js/reference.js
+++ js/reference.js
@@ -1,34 +1,64 @@
-$(document).ready(function(){
+$(document).ready(function () {
+
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+        } else {
+            //x.innerHTML = "Geolocation is not supported by this browser.";
+        }
+    }
+
+    function showPosition(position) {
+        // got lat and lang. Send to the service
+        console.log("Coordds"+data)
+        var data = position.coords;
+        /*
+        $.ajax({
+            url: "/services",
+            data: data,
+            success: onDataResult,
+            dataType: 'json'
+        });
+        */
+        
+    }
     
-    console.log("reference loaded");
-    
-    function toggle(itemClass){
-        console.log("toggle")
+    function onDataResult(result)
+    {
+        // add to the map and the page
+    }
+
+    function toggle(itemClass) {
         var items = $(itemClass);
-        console.log(items)
-        items.each(function(index,value){
-            if($(this).hasClass('showing')){
+        items.each(function (index, value) {
+            if ($(this).hasClass('showing')) {
                 $(this).removeClass('showing').addClass('hiding');
-            }else{
+            } else {
                 $(this).removeClass('hiding').addClass('showing');
             }
-            
+
         })
     }
-    $('.financial').click(function(e){
-       toggle('.financial-item');
-        
-    })
     
-    $('.budget').click(function(e){
+    /*
+    Get position 
+    */ 
+    getLocation();
+    
+    $('.financial').click(function (e) {
+        toggle('.financial-item');
+
+    })
+
+    $('.budget').click(function (e) {
         toggle('.budget-item');
     })
-    
-    $('.housing').click(function(e){
+
+    $('.housing').click(function (e) {
         toggle('.housing-item');
     })
-    $('.signin').click(function(e){
+    $('.signin').click(function (e) {
         toggle('.signin-item');
     })
-    
+
 })
